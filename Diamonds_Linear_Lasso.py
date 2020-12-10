50a64f163e7d055fe15aed6d492337b7debb8708
Nikhil Ajgaonkar <nikhil.aj24@gmail.com>
2020-12-10 12:01:24 -0500

    Update Diamonds_Linear_Lasso.py

--- Diamonds_Linear_Lasso.py
+++ Diamonds_Linear_Lasso.py
@@ -44,31 +44,33 @@ X_test = np.c_[np.ones((np.shape(X_test)[0],1)),X_test]
 #CF = sum(y-y_predicted)^2 + lambda(sum(beta))
 #y_predicted = beta*x and lambda is the lagrange multiplier
 #We will use Gradient Descent method, calculate the derivative of CF when beta is +ve/-ve and update weights
-#Here I take the value of alpha to be 0.01. A more efficient way is to iterate over different values of alpha, calculate the r2 score and select the alpha for the best r2 score.
+#You can take the value of lambda to be 0.01. An efficient way is to iterate over different values of lambda, calculate the r2 score and select the lambda for the best r2 score.
 #Lasso regression using GD
-epochs = 5000
-alpha = 0.1
+epochs = 5000 #Total iterations for GD
+alpha = 0.1 #Step size for GD
 N = len(X_train)
-lm = np.linspace(0.01,20,10)
-beta_GD = np.zeros((np.shape(X_train)[1],len(lm)))
-del_beta_GD = np.zeros((np.shape(X_train)[1],len(lm)))
-CF= np.zeros((epochs,len(lm)))
-r2_score = np.zeros(len(lm))
+lm = np.linspace(0.01,20,10) #Different values of lambda
+beta_GD = np.zeros((np.shape(X_train)[1],len(lm)))#initialize the shape of constants for each lambda =(betas,lambdas)
+del_beta_GD = np.zeros((np.shape(X_train)[1],len(lm)))#initialize the shape of derivative of constants for each lambda =(del betas,lambdas)
+CF= np.zeros((epochs,len(lm)))#initialize the cost function array for each lambda
+r2_score = np.zeros(len(lm))#initialize r2 score for each lambda
 l=0
-for l in range (0,len(lm)):
+for l in range (0,len(lm)):#loop over lambda
  lmda = lm[l]
  e=0
- for e in range (0,epochs):
-        Y_pred = np.matmul(X_train,beta_GD[:,l])
+ for e in range (0,epochs):#different iterations
+        Y_pred = np.matmul(X_train,beta_GD[:,l])#loop over betas
         j=0
         for j in range(0,len(beta_GD[:,l])):
             if beta_GD[j,l]>0:
                 del_beta_GD[j,l] = ((2/N)*(np.matmul(-X_train.T,(y_train-Y_pred)) + lmda))[j]
             else:
                 del_beta_GD[j,l] = ((2/N)*(np.matmul(-X_train.T,(y_train-Y_pred)) - lmda))[j]
-        CF[e,l] = (np.sum((y_train-Y_pred)**2)+lmda*np.sum(beta_GD[:,l]))/(2*len(y_train))
-        beta_GD[:,l] = beta_GD[:,l] - alpha*del_beta_GD[:,l]
+        CF[e,l] = (np.sum((y_train-Y_pred)**2)+lmda*np.sum(beta_GD[:,l]))/(2*len(y_train))#store Cost function for each lambda
+        beta_GD[:,l] = beta_GD[:,l] - alpha*del_beta_GD[:,l]#store coefficients for each lambda
         Y_p = np.matmul(X_test,beta_GD[:,l])
-        r2_score[l] = metrics.r2_score(y_test,Y_p)
+        r2_score[l] = metrics.r2_score(y_test,Y_p)#calculate r2 score for each lambda
+        
+#You can plot r2 score vs lambda and select the best value of lambda. Alo for sanity checks, plot cost function for each lambda as a function of trials to see if it converges towards minumum.
 
 
